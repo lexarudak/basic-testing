9b3689efab10ff402a0986c48ea67cc48ba2cc8c
Aliaksei Rudak <arudak@ebsco.com>
2024-02-09 13:00:19 +0600

    feat: add class tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,110 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+/* eslint-disable prettier/prettier */
+import { getBankAccount, InsufficientFundsError, TransferFailedError, SynchronizationFailedError } from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const initialBalance = 100
+    const account = getBankAccount(initialBalance)
+    expect(account.getBalance()).toEqual(initialBalance)
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const initialBalance = 100
+    const account = getBankAccount(initialBalance)
+    try {
+      account.withdraw(initialBalance + 5)
+    } catch (e) {
+      expect(e).toEqual(new InsufficientFundsError(account.getBalance()))
+    }
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const initialBalance = 100
+    const account = getBankAccount(initialBalance)
+    const secAcc = getBankAccount(0)
+    try {
+      account.transfer(initialBalance + 5, secAcc)
+    } catch (e) {
+      expect(e).toEqual(new InsufficientFundsError(account.getBalance()))
+    }
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const initialBalance = 100
+    const account = getBankAccount(initialBalance)
+    try {
+      account.transfer(initialBalance, account)
+    } catch (e) {
+      expect(e).toEqual(new TransferFailedError())
+    }
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const initialBalance = 100
+    const deposit = 20
+    const account = getBankAccount(initialBalance)
+    account.deposit(deposit)
+    expect(account.getBalance()).toEqual(initialBalance + deposit)
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const initialBalance = 100
+    const withdrawSum = 20
+    const account = getBankAccount(initialBalance)
+    account.withdraw(withdrawSum)
+    expect(account.getBalance()).toEqual(initialBalance - withdrawSum)
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const initialBalance = 100
+    const account = getBankAccount(initialBalance)
+    const secAcc = getBankAccount(0)
+    account.transfer(initialBalance, secAcc)
+    expect(secAcc.getBalance()).toEqual(initialBalance)
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const initialBalance = 100
+    const account = getBankAccount(initialBalance)
+
+    const checkBalance = async (): Promise<number> => {
+        const ans = await account.fetchBalance()
+        return ans || await checkBalance()
+    }
+    const balance = await checkBalance()
+    expect(typeof balance === "number").toBeTruthy()
+    
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const initialBalance = -1
+    const account = getBankAccount(initialBalance)
+
+    const checkBalance = async () => {
+      try {
+        await account.synchronizeBalance()
+      } catch {
+        await checkBalance()
+      }
+    }
+    await checkBalance()
+    expect(account.getBalance()).not.toEqual(initialBalance)
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    const initialBalance = -1
+    const account = getBankAccount(initialBalance)
+    let error
+
+    const checkBalance = async () => {
+      try {
+        await account.synchronizeBalance()
+        await checkBalance()
+      } catch(e) {
+        error = e
+      }
+    }
+    await checkBalance()
+    expect(error).toEqual(new SynchronizationFailedError())
   });
 });
